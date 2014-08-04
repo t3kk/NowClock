9130b16a98c0571779c46af6ea284d1bb6627ca8
Ryan Flaherty <Ryan.Flaherty2@gmail.com>
2014-08-03 21:24:43 -0500

    add comments

--- app/src/main/java/com/ragglefraggle/polarclock/MainActivity.java
+++ app/src/main/java/com/ragglefraggle/polarclock/MainActivity.java
@@ -18,17 +18,26 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Put in the xkcd icon as a placeholder
         ImageView imgView1 = (ImageView) findViewById(R.id.image1);
         Drawable drawable = getResources().getDrawable(R.drawable.polarclock);
         imgView1.setImageDrawable(drawable);
 
+        //Load the earth image
         ImageView imgView2 = (ImageView) findViewById(R.id.image2);
         Drawable drawable2 = getResources().getDrawable(R.drawable.earth);
+        imgView2.setImageDrawable(drawable2);
 
+        //Scale it to fit inside of the xkcd icon.
         imgView2.setScaleX(scale);
         imgView2.setScaleY(scale);
 
-        imgView2.setImageDrawable(drawable2);
+        /*Build a rotation to apply to the image view.
+        it will start at 0 degrees and rotate to 360 and then start again.
+        0.5f means .5 in float, which in this case is referring to the center of the x or y axis.
+        Right now it seems to pause before the animation restarts because of how the frame rate lines up with the duration of the animation and our rotation
+        If we were to do some calculations we can avoid this.
+        */
         RotateAnimation rotateAnimation = new RotateAnimation(0f, 360f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f );
         rotateAnimation.setDuration(1200);
         rotateAnimation.setRepeatCount(-1);
