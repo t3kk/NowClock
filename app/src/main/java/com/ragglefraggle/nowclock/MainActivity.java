847203a8320f1b07b35776c7fdcd4f4cd345b7fc
Ryan Flaherty <Ryan.Flaherty2@gmail.com>
2014-08-06 21:32:20 -0500

    SPLODE

--- app/src/main/java/com/ragglefraggle/nowclock/MainActivity.java
+++ app/src/main/java/com/ragglefraggle/nowclock/MainActivity.java
@@ -1,16 +1,22 @@
 package com.ragglefraggle.nowclock;
 
 import android.app.Activity;
+import android.graphics.Matrix;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
 public class MainActivity extends Activity {
 
+    private static final int MINUTES_IN_A_DAY = 60*24;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +28,24 @@ public class MainActivity extends Activity {
         Drawable drawable = getResources().getDrawable(R.drawable.time_transparent);
         imgView1.setImageDrawable(drawable);
 
+
+
         //Load the earth image
         ImageView imgView2 = (ImageView) findViewById(R.id.image2);
         Drawable drawable2 = getResources().getDrawable(R.drawable.world_transparent);
         imgView2.setImageDrawable(drawable2);
+        Log.d("WOOO", "PivotX: "+imgView2.getPivotX()+" PivotY "+imgView2.getPivotY());
+
+        //Rotate the earth to match current time
+        Matrix matrix=new Matrix();
+        imgView2.setScaleType(ImageView.ScaleType.MATRIX);   //required
+        matrix.postRotate(calculateRotationOffset());
+        imgView2.setImageMatrix(matrix);
+
+
+
+
+
 
 
         /*Build a rotation to apply to the image view.
@@ -35,7 +55,7 @@ public class MainActivity extends Activity {
         If we were to do some calculations we can avoid this.
         */
         Animation linearRotateAnimation = AnimationUtils.loadAnimation(this, R.anim.linear_rotation);
-        imgView2.startAnimation(linearRotateAnimation);
+        //imgView2.startAnimation(linearRotateAnimation);
     }
 
     @Override
@@ -61,7 +81,17 @@ public class MainActivity extends Activity {
 //            helloWorldTextView.setText(helloWorldTextView.getText() + " EXTRA!");
 //        }
 
-
-
+    private float calculateRotationOffset(){
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        long now = cal.getTimeInMillis();
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        long minutesPassedMidnight = now - cal.getTimeInMillis();
+        double fractionOfDay = (double)minutesPassedMidnight/(double)MINUTES_IN_A_DAY;
+        float degrees = (float)(fractionOfDay*360);
+        return degrees;
+    }
 
 }
